13577505d3140a8c5b2b38050a2383c690e71e03
Natan Elkutski <natanelkutski@gmail.com>
2025-10-30 19:43:08 +0200

    second commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const User = require('./models/User');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -29,6 +29,10 @@ mongoose
 
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.post('/api/signup', async (req, res) => {
